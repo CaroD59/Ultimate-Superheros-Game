de7bbf10d5847428698661a1cc3907adeeebec0f
Hochart-dev <hochart.dev@gmail.com>
2021-04-28 18:35:58 +0200

    add Cpupuhero

--- src/components/Battle/Battle.jsx
+++ src/components/Battle/Battle.jsx
@@ -1,12 +1,30 @@
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export default function Battle({ heroToFight }) {
+  const [CpuHero, setCpuHero] = useState({});
+  useEffect(() => {
+    const randomID = Math.floor(Math.random() * 730);
+    axios
+      .get(`https://superheroapi.com/api.php/4118415658211107/${randomID}`)
+      .then(({ data }) => {
+        setCpuHero(data);
+      });
+  }, []);
+
   return (
     <>
-      <img src={heroToFight.image.url} alt="" className="myHeroImg" />
+      {CpuHero.image && (
+        <>
+          <img src={heroToFight.image.url} alt="" className="myHeroImg" />
+          <img src={CpuHero.image.url} alt="" className="myHeroImg" />
+        </>
+      )}
     </>
   );
 }
+
 Battle.propTypes = {
   heroToFight: PropTypes.shape,
 };
